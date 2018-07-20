87bf9b9331d34113a412138d4e65ce660128d7f3
ash <ash@strategicdata.com.au>
2018-07-20 14:15:07 +1000

    Fix release script. Add surge function!

--- release.sh
+++ release.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# If you don't have surge installed, run from docker.
+surge() {
+  docker run -it --rm -v "$PWD:$PWD" -w "$PWD" amurf/surge surge $@
+}
+
 docker-compose run release
-surge dist
+surge --domain sfv.surge.sh dist
